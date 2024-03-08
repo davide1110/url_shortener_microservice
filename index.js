2b3abd7c922f0706462638a1d651b263b91c08e7
davide1110 <davidegrasso200068@gmail.com>
2024-03-08 16:26:24 +0100

    fix short url method

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+let urls = new Map();
+const MAX = 10;
+
 app.use(cors({optionsSuccessStatus: 200}));
 
 app.listen(3000, () => {
@@ -23,16 +26,28 @@ app.post('/api/shorturl', (req, res) => {
         res.json({ error: 'invalid url'});
         return;
     }
-    //console.log(bodyParser.apply(req))
-    res.json({original_url: url, short_url: ""});
+    let key = getRandomNumber();
+    urls.set(key, url);
+    res.json({original_url: url, short_url: key});
+});
+function getRandomNumber() {
+    return Math.floor(Math.random() * MAX);
+
+}
+
+app.get('/api/shorturl/?:url', (req, res) => {
+   // console.log(urls);
+   let param = parseInt(req.params.url);
+    if(urls.has(param)) {
+    res.redirect(urls.get(param));
+    return;    
+}
+   res.json({error: `short url not found with param ${param}`});
 });
 
 function isValidUrl(url) {
     let protocolRegExp = new RegExp("(?:http:\/\/www|https:\/\/www)");
     let domainRegExp = new RegExp("(?:.com|.eu.|.it)");
     let result = protocolRegExp.test(url) && domainRegExp.test(url);
-    //console.log(regexp.exec(url));
-    console.log(result);
-    //use regexp
     return result;
 }
